381cd8914507b3fb7f276f5c901a0a89377a7118
Lucas Cartaxo <lucascartaxor@gmail.com>
2013-07-22 21:48:19 -0300

    cleanup no need to rotate screen, nor change gravity

--- src/br/com/lucascartaxo/box2d/Box2dTestActivity.java
+++ src/br/com/lucascartaxo/box2d/Box2dTestActivity.java
@@ -25,9 +25,6 @@ public class Box2dTestActivity extends Activity {
 	private TestView view;
 	private int rotation;
 	
-	private static final String KEY_DEFAULT_LANDSCAPE = "pl.mg6.testing.box2d.default_landscape";
-	private boolean defaultLandscape;
-	
 	private static final String PREF_ROTATION = "pref.key.rotation";
 	private static final String PREF_GRAVITY = "pref.key.gravity";
 	
@@ -57,34 +54,11 @@ public class Box2dTestActivity extends Activity {
 			w.create(grav);
 		}
 		view.setModel(w);
-		if (savedInstanceState != null) {
-			defaultLandscape = savedInstanceState.getBoolean(KEY_DEFAULT_LANDSCAPE);
-			
-		} else {
-			defaultLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
-			
-			rotation = getPreferences(Context.MODE_PRIVATE).getInt(PREF_ROTATION, Surface.ROTATION_0);
-			int orientation;
-			if (defaultLandscape && rotation == Surface.ROTATION_90
-					|| !defaultLandscape && rotation == Surface.ROTATION_180) {
-				orientation = 9;
-			} else if (defaultLandscape && rotation == Surface.ROTATION_180
-					|| !defaultLandscape && rotation == Surface.ROTATION_270) {
-				orientation = 8;
-			} else if (defaultLandscape && rotation == Surface.ROTATION_270
-					|| !defaultLandscape && rotation == Surface.ROTATION_0) {
-				orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-			} else {
-				orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-			}
-			setRequestedOrientation(orientation);
-		}
 	}
 	
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
-		outState.putBoolean(KEY_DEFAULT_LANDSCAPE, defaultLandscape);
 	}
 	
 	@Override
@@ -140,50 +114,11 @@ public class Box2dTestActivity extends Activity {
 		case R.id.item_clear:
 			removeObjects();
 			return true;
-		case R.id.item_rotate:
-			rotateScreen();
-			return true;
-		case R.id.item_gravity_normal: {
-			SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-			sensorManager.unregisterListener(listener);
-			w.getWorld().setGravity(new Vec2(0.0f, -10.0f));
-			saveGravity(GRAVITY_NORMAL);
-			return true;
-		}
-		case R.id.item_gravity_moon: {
-			SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-			sensorManager.unregisterListener(listener);
-			w.getWorld().setGravity(new Vec2(0.0f, -1.67f));
-			saveGravity(GRAVITY_MOON);
-			return true;
-		}
-		case R.id.item_gravity_zero: {
-			SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-			sensorManager.unregisterListener(listener);
-			w.getWorld().setGravity(new Vec2(0.0f, 0.0f));
-			saveGravity(GRAVITY_ZERO);
-			return true;
-		}
-		case R.id.item_gravity_accelerometer:
-			SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-			Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-			if (sensor != null) {
-				sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-			}
-			saveGravity(GRAVITY_ACCELEROMETER);
-			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
-	private void saveGravity(int newGravity) {
-		gravity = newGravity;
-		Editor editor = getPreferences(MODE_PRIVATE).edit();
-		editor.putInt(PREF_GRAVITY, gravity);
-		editor.commit();
-	}
-	
+
 	private void removeObjects() {
 		Body body = w.getWorld().getBodyList();
 		while (body != null) {
@@ -193,40 +128,6 @@ public class Box2dTestActivity extends Activity {
 				w.getWorld().destroyBody(current);
 			}
 		}
-	}
-	
-	private void rotateScreen() {
-		int orientation;
-		if (defaultLandscape) {
-			if (rotation == Surface.ROTATION_0) {
-				if (Build.VERSION.SDK_INT >= 9) {
-					orientation = 9;
-				} else {
-					orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-				}
-			} else if (rotation == Surface.ROTATION_90) {
-				orientation = 8;
-			} else if (rotation == Surface.ROTATION_180) {
-				orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-			} else {
-				orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-			}
-		} else {
-			if (rotation == Surface.ROTATION_0) {
-				orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-			} else if (rotation == Surface.ROTATION_90) {
-				if (Build.VERSION.SDK_INT >= 9) {
-					orientation = 9;
-				} else {
-					orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-				}
-			} else if (rotation == Surface.ROTATION_180) {
-				orientation = 8;
-			} else {
-				orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-			}
-		}
-		setRequestedOrientation(orientation);
 	}
 
 	private final SensorEventListener listener = new SensorEventListener() {
